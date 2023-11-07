197da9a1e375de21513f233eb44ddf5fb180421c
Ahmad Ali Tresvant <ahmadtresvant@Alis-M1-Pro.local>
2023-11-07 12:29:40 -0500

    created files for project

---
+++ index.js
@@ -0,0 +1,14 @@
+//Refrences 
+const form = document.querySelector(`form`);
+const ol = document.querySelector(`ol`);
+const input = document.querySelector(`input`);
+
+//event listners
+
+form.addEventListener = (`submit`, async (event) => {
+  event.preventDefault();
+});
+
+ol.addEventListener = (`click`, async (event) => {
+  event.preventDefault();
+})
